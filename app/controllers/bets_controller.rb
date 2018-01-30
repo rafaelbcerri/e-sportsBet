e1b9ae478a7e23b3bcbba8ee3a8c616f5e77be62
Rafael Cerri <rafaelbcerri@gmail.com>
2018-01-30 16:06:03 -0200

    Create update user flux

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -1,4 +1,7 @@
 class BetsController < ApplicationController
   def index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,19 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   def destroy
     resource.discard
-
     Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
     set_flash_message :notice, :destroyed if is_flashing_format?
     redirect_to root_url
+  end
+
+  protected
+
+  def update_resource(resource, params)
+    return super if params["password"]&.present?
+    resource.update_without_password(params.except("current_password"))
+  end
+
+  def after_update_path_for(resource)
+    bets_path(resource)
   end
 end
