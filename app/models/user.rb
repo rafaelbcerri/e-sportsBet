5f4f049c893f84b66be77f394c192fff1bf2d87c
Rafael Birochi Cerri <rafaelbcerri@hotmail.com>
2018-02-05 23:23:54 -0200

    Block young users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,22 @@
 class User < ApplicationRecord
+  include Discard::Model
+  include Users::RegistrationsHelper
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
 
    def self.from_omniauth(auth)
-     puts "-----" * 10
-     puts "from_omniauth"
-     puts auth
-     puts "-----" * 10
-     puts auth.inspect
+     return "younger" if user_age(auth.extra.birthday.to_date) < 18
+
      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
        user.email = auth.info.email
        user.password = Devise.friendly_token[0,20]
        user.name = auth.info.name
        user.image = auth.info.image
+       user.birthday = auth.extra.birthday
      end
    end
-
-   include Discard::Model
 
    default_scope { kept }
 
